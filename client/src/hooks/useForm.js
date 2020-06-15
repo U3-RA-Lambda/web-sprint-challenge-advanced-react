4781341dfc99ff50f7dd0c7a7b56c70ae6016729
Michelle Hey <miscmiche@gmail.com>
2020-06-15 19:40:33 -0400

    Edits made to useForm.

--- client/src/hooks/useForm.js
+++ client/src/hooks/useForm.js
@@ -1,3 +1,14 @@
+import { useState } from 'react';
+
+export const useForm = (initialValues) => {
+  const [values, setValues] = useState(initialValues);
+  const handleChanges = (e) => {
+    setValues({...values, [e.target.name]: e.target.value });
+  };
+  
+  return [values, handleChanges];
+}
+
 // // write your custom hook here to control your checkout form
 // #### Checkout Form
 
