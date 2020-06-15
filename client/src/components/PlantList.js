75518d45907b58fd8f8c0ddc36b6e337a36f25d0
Michelle Hey <miscmiche@gmail.com>
2020-06-15 19:25:05 -0400

    Axios call completed in plantList

--- client/src/components/PlantList.js
+++ client/src/components/PlantList.js
@@ -1,24 +1,22 @@
-import React, { Component } from "react";
-import axios from "axios";
+import React, { Component } from 'react';
+import axios from 'axios';
 
 export default class PlantList extends Component {
-  // add state with a property called "plants" - initialize as an empty array
+  state = {
+    plants: []
+  };
 
-  // when the component mounts:
-  //   - fetch data from the server endpoint - http://localhost:3333/plants
-  //   - set the returned plants array to this.state.plants
-
-  /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
+  componentDidMount() {
+    return (
+      axios
+        .get('http://localhost:3333/plants')
+        .then(res => this.setState({ plants: this.state.plants}) || console.log("results", res.data))
+        .catch(err => console.log("Error: ", err))
+    )
+  };
 
   
-//   #### Plant List
-
-// Display a list of the plants from the server. This should be done in the class component `PlantList`.
-
-// - [ ] In the `PlantList` class component, fetch data from the server you now have running - the data can be fetched from `http://localhost:3333/plants`
-// - [ ] Set the data to a state property called `this.state.plants`
-// - [ ] The render function is already built and styled. Once the data is on the state, you will see the list of plants, and you will have the functionality to add a plant to the cart
-
+  /*********  DON'T CHANGE ANYTHING IN THE RENDER FUNCTION *********/
 
   render() {
     return (
